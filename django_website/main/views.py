8526bbb5954f7f91d63b7fb1101c359551b991dc
Gabriel Franco <gabriel@asimov.academy>
2022-10-21 17:31:03 -0300

    models and templates

--- django_website/main/views.py
+++ django_website/main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>yei ouuh!</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls": ls})
 
-def v1(response):
-    return HttpResponse("<h1>View 1!!</h1>")
+def home(response):
+    return render(response, "main/home.html", {})
+
 
