a1aea4a00e5f857237763b72ef8d8ae6c040212b
Gabriel Franco <gabriel@asimov.academy>
2022-10-24 14:50:03 -0300

    new routes to create list and styling

--- django_website/main/urls.py
+++ django_website/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("<int:id>", views.index, name='index'),
     path("home/", views.home, name="Home"),
+    path("", views.home, name="Home"),
     path("create/", views.create, name="Create"),
 ]
